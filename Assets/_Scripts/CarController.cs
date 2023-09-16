e1988ae76e857c163df6a2d4153e6603bf613369
Robert Moegenburg <r.moegenburg@t-online.de>
2023-09-16 18:07:24 +0200

    SteeringIssued

--- Assets/_Scripts/CarController.cs
+++ Assets/_Scripts/CarController.cs
@@ -1,31 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.MPE;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider LeftWheel;
+    public WheelCollider RightWheel;
+
+    public bool motor;
+    public bool steering;
+}
 public class CarController : MonoBehaviour
 {
+    public List<AxleInfo> axleInfos;
     public static CarController Instance { get; private set; }
     private Vector2 MousePosInput;
-    private float steeringAngle;
+    private float steeringAngle = 30f;
     private Camera _mainCamera;
-    private Vector2 _moveInput;
+    private Vector2 _moveInput = Vector2.zero;
     private Vector2 _lookInput;
     private float _cameraZoom = 30f;
     // WHEELS 
-    
-    
-    [SerializeField] private WheelCollider frontWheelLeft,frontWheelRight, rearWheelLeft,rearWheelRight;
-    [SerializeField]private Transform frontObjectLeft,frontObjectRight, rearObjectLeft,rearObjectRight;
-    public Vector3 eulerTest;
-    float maxFwdSpeed  = -3000;
-    float maxBwdSpeed = 1000f;
-    float gravity  = 9.8f;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
     private bool braked = false;
-    private float  maxBrakeTorque =500; 
-    private Rigidbody rb;
-    public Transform centreofmass;
-    private float maxTorque = 1000;
+    // finds the corresponding visual wheel
+
+
+
     private void Awake()
     {
         if (Instance != null)
@@ -36,66 +43,39 @@ public class CarController : MonoBehaviour
         Instance = this;
         SubscribeToInput();
     }
-    
-    private void Start() {
-        rb = GetComponent<Rigidbody>();
-        rb.centerOfMass  = centreofmass.transform.localPosition;
-    }
-    // Update is called once per frame
-    private void brakeSteering()
+    private void Start()
     {
-        if(!braked)
-        {
-            frontWheelLeft.brakeTorque  = 0;
-            frontWheelRight.brakeTorque  = 0;
-            rearWheelLeft.brakeTorque = 0;
-            rearWheelRight.brakeTorque  = 0;
+        _mainCamera = Camera.main;
+    }
+    private void ApplyLocalPosition(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0) {
+            return;
         }
-        // Speed of car, car will move as you will provide the input to it.
-        rearWheelRight.motorTorque = maxTorque * _moveInput.y;
-        rearWheelLeft.motorTorque = maxTorque  * _moveInput.y;
-
-        // Changing car direction
-        frontWheelLeft.steerAngle = 30 * ( _moveInput.x);
-        frontWheelRight.steerAngle = 30 * _moveInput.x;
+     
+        Transform visualWheel = collider.transform.GetChild(0);
+        collider.GetWorldPose(out Vector3 position, out Quaternion rotation);
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
     }
-    private void HandBrake()
+    private void FixedUpdate()
     {
-        if(braked)
+        float motor = maxMotorTorque * _moveInput.y;
+        float steering = maxSteeringAngle * _moveInput.x;
+
+        foreach (AxleInfo x in axleInfos)
         {
-            rearWheelLeft.brakeTorque = maxBrakeTorque *20;
-            rearWheelRight.brakeTorque  = maxBrakeTorque * 20;
-
-            rearWheelLeft.motorTorque = 0;
-            rearWheelRight.motorTorque = 0;
+            if(x.steering){
+                x.LeftWheel.steerAngle = steering; // lerpNeed
+                x.RightWheel.steerAngle = steering;
+            }
+            if(x.motor){
+                x.LeftWheel.motorTorque = motor;
+                x.RightWheel.motorTorque = motor;
+            }
+            ApplyLocalPosition(x.LeftWheel);
+            ApplyLocalPosition(x.RightWheel);
         }
-    }
-    private void RealTireUpdate()
-    {
-        HandBrake();
-        frontObjectLeft.Rotate(frontWheelLeft.rpm/60*360*Time.deltaTime , 0 ,0);
-        frontObjectRight.Rotate(frontWheelRight.rpm/60*360*Time.deltaTime , 0 ,0);
-        rearObjectLeft.Rotate(rearWheelLeft.rpm/60*360*Time.deltaTime , 0 ,0);
-        rearObjectRight.Rotate(rearWheelRight.rpm/60*360*Time.deltaTime , 0 ,0);
-        // STEERING UPDATE
-        Vector3 temp = frontObjectLeft.localEulerAngles;
-        Vector3 temp1 =  frontObjectRight.localEulerAngles;
-
-        temp.y = frontWheelLeft.steerAngle - ( frontObjectLeft.localEulerAngles.z);
-        frontObjectLeft.localEulerAngles = temp;
-
-        temp1.y =  frontWheelRight.steerAngle - (frontObjectRight.localEulerAngles.z);
-        frontObjectRight.localEulerAngles = temp1;
-
-        eulerTest = frontObjectLeft.localEulerAngles;
-    }
-    void FixedUpdate()
-    {
-        brakeSteering();
-
-    }
-    private void Update() {
-        RealTireUpdate();
     }
     #region Controls
     private void OnMoveInput(InputAction.CallbackContext context)
@@ -103,9 +83,9 @@ public class CarController : MonoBehaviour
         if (context.performed)
         {
             _moveInput = context.ReadValue<Vector2>();
-            
+
         }
-        
+
     }
     private void OnLookInput(InputAction.CallbackContext context)
     {
@@ -119,7 +99,7 @@ public class CarController : MonoBehaviour
         if (context.performed)
         {
             MousePosInput = context.ReadValue<Vector2>();
-            
+
         }
     }
     private void OnZoomInput(InputAction.CallbackContext context)
@@ -139,11 +119,11 @@ public class CarController : MonoBehaviour
     }
     private void OnHandbrakeInput(InputAction.CallbackContext context)
     {
-        if(context.started)
+        if (context.started)
         {
             braked = true;
         }
-        if(context.canceled)
+        if (context.canceled)
         {
             braked = false;
         }
